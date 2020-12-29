441c7dc01eac613852821e1d9636322696fa488c
David Dugle <david.dugleii@yahoo.com>
2020-12-29 17:33:05 -0500

    set up a couple of modals with bootstrap. It was a learning process compared to bulma that I was used to

--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,26 @@
 $(document).ready(function() {
-  // This file just does a GET request to figure out which user is logged in
-  // and updates the HTML on the page
-  $.get("/api/user_data").then(function(data) {
-    $(".member-name").text(data.email);
-  });
+//   // This file just does a GET request to figure out which user is logged in
+//   // and updates the HTML on the page
+//   $.get("/api/user_data").then(function(data) {
+//     $(".member-name").text(data.email);
+//   });
+// });
+//this is going to pop out the modal for the note
+// $("#noteAdd").on("show.bs.modal", function(){
+//   console.log("click")
+//   $("#newNote").focus();
+
+$("#noteAdd").on("click", function(){
+  console.log("Clicked");
+  $("#noteModal").modal("show");
+
 });
+$("#medAdd").on("click", function(){
+  console.log("Clicked");
+  $("#medModal").modal("show");
+
+});
+
+
+})
+
