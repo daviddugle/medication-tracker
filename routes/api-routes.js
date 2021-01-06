31e01e8fda50a5804c6cf5e4c9134f3ef04e1f1e
David Dugle <david.dugleii@yahoo.com>
2021-01-06 12:56:39 -0500

    added security features

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,26 +3,26 @@ var db = require("../models");
 var passport = require("../config/passport");
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+  app.post("/api/login", passport.authenticate("local"), function (req, res) {
     return res.sendStatus(200);
   });
 
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
-  app.post("/api/signup", function(req, res) {
+  app.post("/api/signup", function (req, res) {
     db.User.create({
       email: req.body.email,
       password: req.body.password
     })
-      .then(function() {
+      .then(function () {
         res.redirect(307, "/api/login");
       })
-      .catch(function(err) {
+      .catch(function (err) {
         res.status(401).json(err);
       });
   });
@@ -79,66 +79,96 @@ module.exports = function(app) {
       res.sendStatus(403);
     }
   });
-  app.get("/api/medications/:id", function(req, res) {
-    db.Medications.findByPk(req.params.id).then(function(dbMedications) {
-      res.json(dbMedications);
-    });
+  app.get("/api/medications/:id", function (req, res) {
+    if (req.user) {
+      db.Medications.findByPk(req.params.id).then(function (dbMedications) {
+        res.json(dbMedications);
+      });
+    } else {
+      res.sendStatus(403);
+    }
   });
-  app.get("/api/notes/:id", function(req, res) {
-    db.Notes.findByPk(req.params.id).then(function(dbNotes) {
-      res.json(dbNotes);
-    });
+  app.get("/api/notes/:id", function (req, res) {
+    if (req.user) {
+      db.Notes.findByPk(req.params.id).then(function (dbNotes) {
+        res.json(dbNotes);
+      });
+
+    } else {
+      res.sendStatus(403);
+    }
+
   });
   // let's set some put routes so we can edit the items
-  app.put("/api/notes", function(req, res) {
-    console.log(req.body);
-    db.Notes.update(req.body, {
-      where: {
-        id: req.body.id
-      }
-    }).then(function(dbNotes) {
-      res.json(dbNotes);
-    });
+  app.put("/api/notes", function (req, res) {
+    if (req.user){
+      console.log(req.body);
+      db.Notes.update(req.body, {
+        where: {
+          id: req.body.id
+        }
+      }).then(function (dbNotes) {
+        res.json(dbNotes);
+      });
+
+    } else {
+      res.sendStatus(403);
+    }
   });
-  app.put("/api/medications", function(req, res) {
-    console.log(req.body);
-    db.Medications.update(req.body, {
-      where: {
-        id: req.body.id
-      }
-    }).then(function(dbMedications) {
-      res.json(dbMedications);
-    });
+  app.put("/api/medications", function (req, res) {
+    if (req.user){
+      console.log(req.body);
+      db.Medications.update(req.body, {
+        where: {
+          id: req.body.id
+        }
+      }).then(function (dbMedications) {
+        res.json(dbMedications);
+      });
+
+    } else {
+      res.sendStatus(403);
+    }
   });
   // delete routes
-  app.delete("/api/notes/:id", function(req, res) {
-    db.Notes.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbNotes) {
-      res.json(dbNotes);
-    });
+  app.delete("/api/notes/:id", function (req, res) {
+    if (req.user){
+      db.Notes.destroy({
+        where: {
+          id: req.params.id
+        }
+      }).then(function (dbNotes) {
+        res.json(dbNotes);
+      });
+
+    } else {
+      res.sendStatus(403);
+    }
   });
   app.delete("/api/medications/:id", function (req, res) {
-    db.Medications.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbMedications) {
-      res.json(dbMedications);
-    });
+    if (req.user){
+      db.Medications.destroy({
+        where: {
+          id: req.params.id
+        }
+      }).then(function (dbMedications) {
+        res.json(dbMedications);
+      });
+
+    } else {
+      res.sendStatus(403);
+    }
   });
 
 
   // Route for logging user out
-  app.get("/logout", function(req, res) {
+  app.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/");
   });
 
   // Route for getting some data about our user to be used client side
-  app.get("/api/user_data", function(req, res) {
+  app.get("/api/user_data", function (req, res) {
     if (!req.user) {
       // The user is not logged in, send back an empty object
       res.json({});
@@ -154,6 +184,6 @@ module.exports = function(app) {
   app.get("/api/user_data", isAuthenticated, (req, res) => {
     if (req.user) {
       res.render("members");
-    } 
+    }
   });
 };
